74c3f357c4dd38981cffe6c82ce4330aa7fb9f83
Goncalo-Faria <gonca2372@gmail.com>
2018-11-21 22:12:06 +0000

    dwdq

--- Description/Controlo.sql
+++ Description/Controlo.sql
@@ -7,10 +7,17 @@ CREATE USER 'organizador'@'localhost' IDENTIFIED BY 'organizadorpassword';
 # Especificar em detalhe quais os procedures
 GRANT CALL ON Workbench.* TO 'organizador'@'localhost;
 
-GRANT SELECT ON (Workbench.Locais,Workbench.Plataforma) TO 'organizador86264'@'localhost;
+GRANT SELECT ON (Workbench.Locais,Workbench.Plataforma) TO 'organizador'@'localhost;
 
 ###
 
 CREATE USER 'participante'@'localhost' IDENTIFIED BY 'participantepassword';
 
 GRANT CALL ON Workbench.ClassificaBilhete TO 'participante'@'localhost;
+
+###
+
+CREATE USER 'Admin'@'localhost' IDENTIFIED BY 'Adminpassword';
+
+GRANT ALL PRIVILEGES ON Workbench.* TO 'Admin'@'localhost;
+
