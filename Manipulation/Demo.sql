489a50d4caa09d6752171ed4709a198f176f03dd
Goncalo-Faria <gonca2372@gmail.com>
2018-11-22 11:26:48 +0000

    ate À 7 querie

--- Manipulation/Demo.sql
+++ Manipulation/Demo.sql
@@ -1,11 +1,85 @@
-USE EventsWorkbench;
+USE EventsWorkbench;# RE1 arg:numorg
+Select distinct
+    Ent.nome, Ent.endereco, Ent.email, Ent.telemovel
+From
+    Evento as E,
+    Organizador as O,
+    Organizador_has_Evento as OE,
+    PermiteEntrada_Evento_Participante_Divulgacao as PEPD,
+    Participante as P,
+	Entidade as Ent
+where
+    E.id = OE.Evento_id
+    and OE.Organizador_Entidade_id = numorg
+    and PEPD.Evento_id = E.id
+    and P.Entidade_id = PEPD.Participante_Entidade_id
+	and Ent.id = P.Entidade_id;
+# RE2 arg:numorg
+Select distinct
+    P.*
+From
+    Evento as E,
+    Organizador as O,
+    Organizador_has_Evento as OE,
+    Divulgacao as D,
+    Plataforma as P
+where
+    E.id = OE.Evento_id
+    and OE.Organizador_Entidade_id = numorg
+    and D.Evento_id = E.id
+    and D.Plataforma_id = P.id;
 
-#listar todos os eventos com quem os comprou, organizou assim como onde vão decorrer
-SELECT L.designacao as Local, E.designacao as Evento, O.designacao as Organizacao
-	FROM Local as L, Evento as E, Organizacao as O, Organizacao_tem_Evento as OE
-	WHERE E.id = OE.Evento_id and O.id = OE.Organizacao_id and L.id = E.Local_id;
+# RE3 arg:numorg
+Select distinct
+    E.*, L.Tipo
+From
+    Evento as E,
+    Organizador as O,
+    Organizador_has_Evento as OE,
+	Local as L
+where
+    E.id = OE.Evento_id
+    and OE.Organizador_Entidade_id = numorg
+	and L.Entidade_id = L.Local_Entidade_id;
 
-# Listar todos os bilhetes com o nome de quem o comprou e o tipo que é.    
-SELECT P.nome as Participante,E.designacao as Evento
-	FROM Participante as P, Evento as E, Bilhete as B
-    WHERE B.Participante_nif = P.nif and E.id = B.Evento_id;
+# RE4 arg:numorg, numevento
+Select distinct
+    E.*
+From
+    Evento as E,
+    Organizador as O,
+    Organizador_has_Evento as OE
+where
+    E.id = OE.Evento_id
+    and OE.Organizador_Entidade_id = numorg
+	and E.id = numevento;
+
+# RE5 numevento
+Select distinct
+    E.id,P.genero,P.nif,P.datadenascimento,Ent.nome, Ent.endereco, Ent.email, Ent.telemovel
+From
+    Evento as E,
+    PermiteEntrada_Evento_Participante_Divulgacao as PEPD,
+    Participante as P,
+	Entidade as Ent
+where
+	E.id = numevento
+    and PEPD.Evento_id = E.id
+    and P.Entidade_id = PEPD.Participante_Entidade_id
+	and Ent.id = P.Entidade_id;
+
+# RE6 numevento
+Select distinct
+    E.id,P.genero,P.nif,P.datadenascimento,Ent.nome, Ent.endereco, Ent.email, Ent.telemovel
+From
+    Evento as E,
+    PermiteEntrada_Evento_Participante_Divulgacao as PEPD,
+    Participante as P,
+	Entidade as Ent
+where
+	E.id = numevento
+    and PEPD.Evento_id = E.id
+    and P.Entidade_id = PEPD.Participante_Entidade_id
+	and Ent.id = P.Entidade_id;
+
+#RE7
