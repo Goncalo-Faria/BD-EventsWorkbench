669eda2271e8f7f1dc5267d24a988d951b0b53ff
Goncalo-Faria <gonca2372@gmail.com>
2018-11-22 23:51:46 +0000

    queries apper to be working

--- Description/Povoamento.sql
+++ Description/Povoamento.sql
@@ -1,42 +1,56 @@
 USE EventsWorkbench;
 
-INSERT INTO Local (designacao)
-VALUES 	('Carpe'),
-				('Sardinha'),
-                ('BarDaAssociacao');
+INSERT INTO Entidade (Id,Endereco, Nome,Email,Telemovel)
+VALUES  (1,'ruaa111','a111','a111@mail.com','351999a11'),
+				(2,'ruaa112','a112','a112@mail.com','351999a12'),
+				(3,'ruaa113','a113','a113@mail.com','351999a13'),
+				(4,'ruaa114','a114','a114@mail.com','351999a14'),
+				(5,'ruaa115','a115','a115@mail.com','351999a15'),
+				(6,'ruaa116','a116','a116@mail.com','351999a16'),
+				(7,'ruaa117','a117','a117@mail.com','351999a17'),
+				(8,'ruaa118','a118','a118@mail.com','351999a18'),
+				(9,'ruaa119','a119','a119@mail.com','351999a19'),
+                (10,'ruaa120','a120','a120@mail.com','351999a20');
 
-INSERT INTO Evento (Local_id,designacao)
-VALUES 	(1,'Jantar de Miei'),
-				(3,'Arraial Cesium'),
-                (2,'Casamento do Joel');
 
-INSERT INTO Organizacao (designacao)
-VALUES 	('Mae do Joel'),
-				('Cesium'),
-                ('Comissao MIEI');
+INSERT INTO Local (Entidade_id,Descricao,Tipo,Lotacao)
+VALUES 	(1,'é o Carpe',1,40),
+				(2,'é o Sardinha',1,50),
+                (3,'é o BarDaAssociacao',1,60);
+                
+INSERT INTO Organizador (Entidade_id, Descricao)
+VALUES 	(4,'Mae do Joel'),
+				(5,'Cesium'),
+                (6,'Comissao MIEI');
+                
+INSERT INTO Participante (Entidade_id,nif,DataDeNascimento,Genero)
+VALUES (7,15541190,'1998-3-3',2),
+			   (8,33442,'1998-3-3',2),
+               (9,22,'1998-3-3',2),
+               (10,1888,'1998-3-3',1);
+               
+INSERT INTO Plataforma (id,nome)
+VALUES 	(1,'Facebook'),
+				(2,'Intagram'),
+                (3,'Discord');
 
-INSERT INTO Organizacao_tem_evento (Evento_id,Organizacao_id)
-VALUES 	(1,3),
-				(2,2),
-                (1,2),
-                (3,1);
+INSERT INTO Evento (Id,Nome,Descricao,Tipo,data,DataInicioRegistoParticipantes,DataFimRegistoParticipantes,Duracao,Preco,NumeroMaximoDeParticipantes,Local_Entidade_Id)
+VALUES 	(1,'Jantar de Miei','comes e bebes',1,'1998-3-3','1998-3-3','1998-3-3',CURRENT_TIME(),1.1,10,1),
+				(3,'Arraial Cesium','comes e bebes',1,'1998-3-3','1998-3-3','1998-3-3',CURRENT_TIME(),1.2,20,2),
+                (2,'Casamento do Joel','comes e bebes',1,'1998-3-3','1998-3-3','1998-3-3',CURRENT_TIME(),1.3,30,3);
 
-INSERT INTO Plataforma (designacao)
-VALUES 	('Facebook'),
-				('Intagram'),
-                ('Discord');
+INSERT INTO Organizador_has_evento (Evento_id,Organizador_Entidade_id)
+VALUES 	(1,4),
+				(2,5),
+                (1,5),
+                (3,6);
+                
+##
                 
 INSERT INTO Publicidade(Evento_id,Plataforma_id)
 VALUES (1,1),
 			   (2,1),
                (3,1);
-
-               
-INSERT INTO Participante(nif,nome)
-VALUES (15541190,'Gonçalo Faria'),
-			   (33442,'João Vilaça'),
-               (22,'José Fernandes'),
-               (1888,'Catarina Machado');
                
 INSERT INTO Bilhete (Evento_id,Participante_nif)
 VALUES (1,15541190),(2,15541190),(3,15541190);
--- Manipulation/Demo.sql
+++ Manipulation/Demo.sql
@@ -1,4 +1,22 @@
 USE EventsWorkbench;# RE1 arg:numorg
+
+### Testar
+Select *
+From Entidade;
+
+Select *
+From Local;
+
+Select *
+From Organizador;
+
+Select *
+From Participante;
+
+Select *
+From Evento;
+
+##RE1 arg:numorg
 Select distinct
     Ent.nome, Ent.endereco, Ent.email, Ent.telemovel
 From
@@ -14,6 +32,7 @@ where
     and PEPD.Evento_id = E.id
     and P.Entidade_id = PEPD.Participante_Entidade_id
 	and Ent.id = P.Entidade_id;
+    
 # RE2 arg:numorg
 Select distinct
     P.*
@@ -29,7 +48,13 @@ where
     and D.Evento_id = E.id
     and D.Plataforma_id = P.id;
 
-# RE3 arg:numorg
+# RE3 arg:numorg = 5
+## ESTÁ A FUNCIONAR
+## ESTÁ A FUNCIONAR
+## ESTÁ A FUNCIONAR
+## ESTÁ A FUNCIONAR
+## ESTÁ A FUNCIONAR
+## ESTÁ A FUNCIONAR
 Select distinct
     E.*, L.Tipo
 From
@@ -39,10 +64,16 @@ From
 	Local as L
 where
     E.id = OE.Evento_id
-    and OE.Organizador_Entidade_id = numorg
-	and L.Entidade_id = L.Local_Entidade_id;
+    and OE.Organizador_Entidade_id = 5
+	and L.Entidade_id = L.Entidade_id;
 
-# RE4 arg:numorg, numevento
+# RE4 arg:numorg=4, numevento=1
+## ESTÁ A FUNCIONAR
+## ESTÁ A FUNCIONAR
+## ESTÁ A FUNCIONAR
+## ESTÁ A FUNCIONAR
+## ESTÁ A FUNCIONAR
+## ESTÁ A FUNCIONAR
 Select distinct
     E.*
 From
@@ -51,10 +82,11 @@ From
     Organizador_has_Evento as OE
 where
     E.id = OE.Evento_id
-    and OE.Organizador_Entidade_id = numorg
-	and E.id = numevento;
+    and OE.Organizador_Entidade_id = 4
+	and E.id = 1;
 
 # RE5 numevento
+
 Select distinct
     E.id,P.genero,P.nif,P.datadenascimento,Ent.nome, Ent.endereco, Ent.email, Ent.telemovel
 From
@@ -82,4 +114,69 @@ where
     and P.Entidade_id = PEPD.Participante_Entidade_id
 	and Ent.id = P.Entidade_id;
 
-#RE7
+#RE7 Data: dinf, dsup
+Select *
+From 
+	Evento as E
+where 
+	Evento.data >= dinf 
+	and Evento.data <= dsup;
+
+#RE7 idLocal :idl=2
+## ESTÁ A FUNCIONAR
+## ESTÁ A FUNCIONAR
+## ESTÁ A FUNCIONAR
+## ESTÁ A FUNCIONAR
+## ESTÁ A FUNCIONAR
+## ESTÁ A FUNCIONAR
+Select *
+From 
+	Evento as E
+where 
+	E.Local_Entidade_Id = 2;
+
+#RE8
+Select
+	D.tipo, count(D.tipo)
+From 
+	Divulgacao as D,
+	PermiteEntrada_Evento_Participante_Divulgacao as PEPD
+where
+	PEPD.Divulgacao_id = D.id 
+group by D.tipo
+order by count(D.tipo);
+
+Select
+	D.tipo, (sum(D.Preco)/count(D.tipo))
+From 
+	Divulgacao as D,
+	PermiteEntrada_Evento_Participante_Divulgacao as PEPD
+where
+	PEPD.Divulgacao_id = D.id 
+group by D.tipo
+order by (sum(D.Preco)/count(D.tipo));
+
+#RE9 arg: div
+
+Select 
+	D.*
+From 
+	Divulgacao as D
+where
+	D.id = divid;
+    
+#RE10
+
+Select 
+	D.*, count(PEPD.Participante_Entidade_Id)
+From 
+	Divulgacao as D,
+    PermiteEntrada_Evento_Participante_Divulgacao as PEPD
+where
+	PEPD.Divulgacao_id = D.id
+	and D.id = divid
+group by Divulgacao_id
+
+
+
+
