e70923f6e05a0cba445d8222429e77455087eaa7
Catarina Machado <catarinamachado11@gmail.com>
2018-11-25 12:40:45 +0000

    some fixes

--- Description/Transaction.sql
+++ Description/Transaction.sql
@@ -1,6 +1,6 @@
 USE EventsWorkbench;
 
-# 1. Adicionar Local
+-- 1. Adicionar Local
 DELIMITER $$
 CREATE PROCEDURE addLocal(IN id INT, IN endereco VARCHAR(128), IN nome VARCHAR(64), 
 														IN email VARCHAR(45), IN telemovel VARCHAR(15),
@@ -8,10 +8,13 @@ CREATE PROCEDURE addLocal(IN id INT, IN endereco VARCHAR(128), IN nome VARCHAR(6
 														IN tipo ENUM('Bares‎', 'Casas noturnas', 'Casa de espetáculos', 'Hotelaria', 'Restaurantes', 
                                                         'Centros Culturais', 'Multiusos', 'Quintas', 'Outros'))
 BEGIN
-    DECLARE `_rollback` BOOL DEFAULT 0;
-    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET `_rollback` = 1;
+        
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION 
+    BEGIN
+        ROLLBACK;
+    END;
     
-    START TRANSACTION;
+	START TRANSACTION;
 		-- criar Entidade
         INSERT INTO Entidade (id, endereco, nome, email, telemovel)
 		VALUES (id, endereco, nome, email, telemovel);
@@ -19,21 +22,17 @@ BEGIN
 		INSERT INTO Local (entidade_id, lotacao, descricao, tipo)
 		VALUES (id, lotacao, descricao, tipo);
         
-		IF `_rollback` THEN
-			ROLLBACK;
-		ELSE
-			COMMIT;
-		END IF;
+		COMMIT;
     
 END $$
 DELIMITER ;
 
 /* TESTE
-CALL addLocal(90, "London", "Caty", "catarina8787er8@gmail.com", "6ere868", 34, "Teste do Local 3", 2);
+CALL addLocal(91, "London", "Caty", "catarinsfa8787er8@gmail.com", "6ereff868", 34, "Teste do Local 3", 2);
 
 SELECT * 
 FROM Entidade
-WHERE id = 90;
+WHERE id = 91;
 */
 
 
@@ -42,8 +41,10 @@ DELIMITER $$
 CREATE PROCEDURE addOrganizador(IN id INT, IN endereco VARCHAR(128), IN nome VARCHAR(64), 
 																   IN email VARCHAR(45), IN telemovel VARCHAR(15), IN descricao TEXT(512))
 BEGIN
-    DECLARE `_rollback` BOOL DEFAULT 0;
-    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET `_rollback` = 1;
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION 
+    BEGIN
+        ROLLBACK;
+    END;
     
     START TRANSACTION;
 		-- criar Entidade
@@ -53,25 +54,21 @@ BEGIN
 		INSERT INTO Organizador(entidade_id, descricao)
 		VALUES (id, descricao);
         
-		IF `_rollback` THEN
-			ROLLBACK;
-		ELSE
-			COMMIT;
-		END IF;
+		COMMIT;
     
 END $$
 DELIMITER ;
 
 /* TESTE
-CALL addOrganizador(900, "London", "Caty", "ola@gmail.com", "999888", "ola esta tudo bem e comigo");
+CALL addOrganizador(901, "London", "Caty", "olagfg@gmail.com", "999fg888", "ola esta tudo bem e comigoooo");
 
 SELECT * 
 FROM Organizador
-WHERE Entidade_Id = 900;
+WHERE Entidade_Id = 901;
 
 SELECT * 
 FROM Entidade
-WHERE id = 900;
+WHERE id = 901;
 */
 
 
@@ -81,8 +78,10 @@ CREATE PROCEDURE addParticipante(IN id INT, IN endereco VARCHAR(128), IN nome VA
 																 IN email VARCHAR(45), IN telemovel VARCHAR(15),  
 																 IN datadenascimento DATE, IN genero ENUM('Feminino', 'Masculino'), IN nif INT)
 BEGIN
-    DECLARE `_rollback` BOOL DEFAULT 0;
-    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET `_rollback` = 1;
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION 
+    BEGIN
+        ROLLBACK;
+    END;
     
     START TRANSACTION;
 		-- criar Entidade
@@ -92,25 +91,21 @@ BEGIN
 		INSERT INTO Participante (entidade_id, datadenascimento, genero, nif)
 		VALUES  (id, datadenascimento, genero, nif);
         
-		IF `_rollback` THEN
-			ROLLBACK;
-		ELSE
-			COMMIT;
-		END IF;
+		COMMIT;
     
 END $$
 DELIMITER ;
 
 /* TESTE
-CALL addParticipante(901, "London", "Caty", "olaaaa@gmail.com", "999sds888", '1998-04-01', 2, 876543234);
+CALL addParticipante(905, "London", "Caty", "olaadfdfaa@gmail.com", "99fdf9sds888", '1998-04-01', 2, 876543234);
 
 SELECT * 
 FROM Participante
-WHERE Entidade_id = 901;
+WHERE Entidade_id = 905;
 
 SELECT * 
 FROM Entidade
-WHERE id = 901;
+WHERE id = 905;
 */
 
 
@@ -127,11 +122,12 @@ BEGIN
 	DECLARE maxlotacao INT;
 	DECLARE `_rollback` BOOL DEFAULT 0;
     DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET `_rollback` = 1;
-	SET maxlotacao = (SELECT Local.lotacao
-									FROM Local
-									WHERE Local.Entidade_Id = local_Entidade_Id);
         
     START TRANSACTION;
+		-- guardar lotacao maxima do local numa variavel
+		SET maxlotacao = (SELECT Local.lotacao
+										FROM Local
+										WHERE Local.Entidade_Id = local_Entidade_Id);
 		-- verificar se o local possui a lotacao maxima necessaria
 		IF (maxlotacao < numeroMaximoDeParticipantes) THEN 
 			SET `_rollback` = 1;
@@ -153,7 +149,7 @@ DELIMITER ;
 
 
 /* TESTE
-CALL addEvento (110,'Quiver','Concerto do Quiver',5,'2017-08-23',CURRENT_TIME(), 34.34,'2017-08-10','2017-08-23',2.4,9,40);
+CALL addEvento (112,'Quiver','Concerto do Quiver',5,'2017-08-23',CURRENT_TIME(), 34.34,'2017-08-10','2017-08-23',2.4,9,40);
 
 INSERT INTO Evento (id, nome, descricao, tipo, data, duracao, preco, dataInicioRegistoParticipantes, dataFimRegistoParticipantes, classificacao, numeroMaximoDeParticipantes, local_Entidade_Id)
 VALUES (105,'Quiver','Concerto do Quiver',5,'2017-08-23',CURRENT_TIME(), 34.34,'2017-08-10','2017-08-23',2.4,900,40);
@@ -164,7 +160,7 @@ WHERE Local.Entidade_Id = 40;
 
 SELECT * 
 FROM EVENTO
-where id = 109;
+where id = 112;
 */
 
 
@@ -180,16 +176,20 @@ BEGIN
     DECLARE nr_participantes_no_evento INT;
     DECLARE `_rollback` BOOL DEFAULT 0;
     DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET `_rollback` = 1;
-	SET evento_divulgacao = (SELECT d.evento_id
-												FROM Divulgacao d
-												WHERE d.id = divulgacao_id);
-	SET nr_participantes_maximo = (SELECT e.NumeroMaximoDeParticipantes
-														 FROM Evento e
-														 WHERE e.id = evento_id);
-    SET nr_participantes_no_evento = (SELECT COUNT(pe.evento_id)
-															  FROM PermiteEntrada_Evento_Participante_Divulgacao pe
-															  WHERE pe.evento_id = evento_id);
+
     START TRANSACTION;
+		-- guardar variavel com o id do evento publicitado na divulgacao
+    	SET evento_divulgacao = (SELECT d.evento_id
+													FROM Divulgacao d
+													WHERE d.id = divulgacao_id);
+        -- numero maximo de participantes permitido evento
+		SET nr_participantes_maximo = (SELECT e.NumeroMaximoDeParticipantes
+															 FROM Evento e
+															 WHERE e.id = evento_id);
+		-- numero de participantes inscritos no evento
+		SET nr_participantes_no_evento = (SELECT COUNT(pe.evento_id)
+																  FROM PermiteEntrada_Evento_Participante_Divulgacao pe
+																   WHERE pe.evento_id = evento_id);
 		-- verificar se o evento associado à divulgação é o mesmo evento que se prende adicionar ao relacionamento ternário
 		IF (evento_divulgacao <> evento_id) THEN 
 			SET `_rollback` = 1;
@@ -213,6 +213,7 @@ DELIMITER ;
 
 /* TESTE
 CALL addParticipanteEventoDivulgacao(8,2,2,6,1, 90, 'L78');
+
 */
 
 # 8. Participante adiciona classificação -> TRIGGER
@@ -292,4 +293,39 @@ WHERE Evento.id = 20;
 
 
 # 9. Apagar uma entrada do relacionamento ternário
+DELIMITER $$
 
+CREATE TRIGGER after_classificacao_delete
+AFTER DELETE ON PermiteEntrada_Evento_Participante_Divulgacao 
+FOR EACH ROW 
+BEGIN
+
+DECLARE nr_classificacoes INT;
+DECLARE ex_media DECIMAL(4,2);
+DECLARE classificacao_deleted INT;
+DECLARE old_somaclassificacao_total INT;
+
+SET nr_classificacoes = (SELECT COUNT(classificacao)
+									     FROM PermiteEntrada_Evento_Participante_Divulgacao
+									     WHERE evento_id = evento_id AND (classificacao IS NOT NULL));
+SET ex_media = (SELECT classificacao
+							 FROM Evento
+							 WHERE id = NEW.evento_id);
+
+SET  classificacao_deleted  = (SELECT classificacao = classificacao
+												  FROM DELETED);
+
+
+IF ex_media IS NOT NULL THEN
+	SET old_somaclassificacao_total = (nr_classificacoes + 1) * ex_media;
+ELSE 
+	SET old_somaclassificacao_total = 0;
+END IF;
+										
+UPDATE Evento
+SET Evento.Classificacao = (old_somaclassificacao_total - NEW.classificacao) / (nr_classificacoes - 1)
+WHERE Evento.id = NEW.evento_id;   
+    
+END $$
+
+DELIMITER ;
