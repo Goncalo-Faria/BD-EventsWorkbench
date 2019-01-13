212031409d3da0b20af7c572c0cdc0d4e23bcfac
Goncalo-Faria <gonca2372@gmail.com>
2019-01-13 23:04:56 +0000

    more general

--- Migration/src/main/java/Migrator.java
+++ Migration/src/main/java/Migrator.java
@@ -10,37 +10,6 @@ import java.util.List;
 
 public class Migrator {
 
-    private static Neo4JNode fillnode(List<String> atributes,
-                                      ResultSet table, String node) throws SQLException{
-        Neo4JNode neo = new Neo4JNode(node);
-        int col = table.getMetaData().getColumnCount();
-
-        for (int i = 1; i <= col; i++)
-            if(atributes.get(i - 1).equals("id") || atributes.get(i - 1).equals("preco"))
-                neo.addAtribute(atributes.get(i - 1), table.getString(i));
-            else
-                neo.addAtribute(atributes.get(i - 1), " '" + table.getString(i) + "' ");
-
-
-        return neo;
-    }
-
-    private static Neo4JRelation fillrelationship(List<String> atributes,
-                                                  ResultSet table, String reltype, String originNodeType,
-                                                  String destNodeType) throws SQLException{
-
-        Neo4JRelation rel = new Neo4JRelation(reltype,
-                originNodeType, table.getString(1) ,
-                destNodeType, table.getString(2));
-
-        int col = table.getMetaData().getColumnCount();
-
-        for (int i = 3; i <= col; i++)
-            rel.addRelationAtribute(atributes.get(i - 3), table.getString(i));
-
-        return rel;
-    }
-
     private static List<Neo4JNode> an = new ArrayList<>();
     private static List<Neo4JRelation> bn = new ArrayList<>();
 
@@ -50,75 +19,40 @@ public class Migrator {
             EventsWorkbenchGetter wb = new EventsWorkbenchGetter(args[0],args[1],args[2]);
             ResultSet table;
 
-            //evento
             List<String> eventlist = new ArrayList<>();
             eventlist.add("id");eventlist.add("nome");//eventlist.add("preco");
-            table = wb.evento();
-            while(table.next())
-                an.add(fillnode(eventlist, table, "Evento"));
+            transfer_table(eventlist, wb.evento(),"Evento");
 
-            //<<<<
-            //organizacao
             List<String> orglist = new ArrayList<>();
             orglist.add("id");orglist.add("nome");orglist.add("email");
-            table = wb.organizador();
-            while(table.next())
-                an.add(fillnode(orglist, table, "Organizador"));
+            transfer_table(orglist, wb.organizador(),"Organizador");
 
-            //<<<<
-            //divulgacao
             List<String> divlist = new ArrayList<>();
             divlist.add("id");divlist.add("tipo");divlist.add("preco");
-            table = wb.divulgacao();
-            while(table.next())
-                an.add(fillnode(divlist, table, "Divulgacao"));
+            transfer_table(divlist, wb.divulgacao(),"Divulgacao");
 
-            //<<<<
-            //<<<<
-            //participacao
             List<String> partlist = new ArrayList<>();
             partlist.add("id");partlist.add("nome");partlist.add("email");partlist.add("telemovel");
             partlist.add("genero");partlist.add("nif");partlist.add("DataDeNascimento");
-            table = wb.participante();
-            while(table.next())
-                an.add(fillnode(partlist, table, "Participante"));
+            transfer_table(partlist,wb.participante(),"Participante");
 
-            //<<<<
-            //<<<<
-            //participa
             List<String> participalist = new ArrayList<>();
             participalist.add("preco");
-            table = wb.participa();
+            transfer_relationship(participalist, wb.participa(),
+                    "Participa", "Participante",
+                    "Evento");
 
-            while(table.next())
-                bn.add(fillrelationship(participalist,table,"Participa",
-                        "Participante","Evento"));
+            transfer_relationship(new ArrayList<>(), wb.organiza(),
+                    "Organiza", "Organizador",
+                    "Evento");
 
-            //<<<<
-            //organiza
-            table = wb.organiza();
+            transfer_relationship(new ArrayList<>(), wb.divulga(),
+                    "Divulga", "Divulgacao",
+                    "Evento");
 
-            while(table.next())
-                bn.add(fillrelationship(new ArrayList<>(),table,"Organiza",
-                        "Organizador","Evento"));
-
-            //<<<<
-            //<<<<
-            //divulga
-            table = wb.divulga();
-
-            while(table.next())
-                bn.add(fillrelationship(new ArrayList<>(), table,"Divulga",
-                        "Divulgacao","Evento"));
-
-            //<<<<
-            //<<<<
-            //influencia
-            table = wb.influencia();
-
-            while(table.next())
-                bn.add(fillrelationship(new ArrayList<>(), table,"Influencia",
-                        "Divulgacao","Participante"));
+            transfer_relationship(new ArrayList<>(), wb.influencia(),
+                    "Influencia", "Divulgacao",
+                    "Participante");
 
             Neo4JWriter nw = new Neo4JWriter(args[3],args[4],args[5]);
             nw.createEntradas(an);
@@ -134,5 +68,50 @@ public class Migrator {
         }catch(ClassNotFoundException b){
             System.out.println(" Driver error ");
         }
+    }
+
+    private static void transfer_relationship(List<String> atributes, ResultSet table,
+                                              String relationship, String from ,
+                                              String to) throws SQLException{
+        while(table.next())
+            bn.add(fillrelationship(atributes ,table ,relationship ,
+                    from ,to ));
+    }
+
+    private static void transfer_table(List<String> atributes, ResultSet table,
+                                       String nodetype) throws SQLException{
+        while(table.next())
+            an.add(fillnode(atributes, table, nodetype));
+    }
+
+    private static Neo4JRelation fillrelationship(List<String> atributes,
+                                                  ResultSet table, String relationship, String from,
+                                                  String to) throws SQLException{
+
+        Neo4JRelation rel = new Neo4JRelation(relationship,
+                from, table.getString(1) ,
+                to, table.getString(2));
+
+        int col = table.getMetaData().getColumnCount();
+
+        for (int i = 3; i <= col; i++)
+            rel.addRelationAtribute(atributes.get(i - 3), table.getString(i));
+
+        return rel;
+    }
+
+    private static Neo4JNode fillnode(List<String> atributes,
+                                      ResultSet table, String node) throws SQLException{
+        Neo4JNode neo = new Neo4JNode(node);
+        int col = table.getMetaData().getColumnCount();
+
+        for (int i = 1; i <= col; i++)
+            if(atributes.get(i - 1).equals("id") || atributes.get(i - 1).equals("preco"))
+                neo.addAtribute(atributes.get(i - 1), table.getString(i));
+            else
+                neo.addAtribute(atributes.get(i - 1), " '" + table.getString(i) + "' ");
+
+
+        return neo;
     }
 }
