fe2a6d6ca1c6af3ff6c77f333b580a06ba9e68a3
Goncalo-Faria <gonca2372@gmail.com>
2019-01-14 11:59:30 +0000

    migrator setup

--- Migration/src/main/java/Migrator.java
+++ Migration/src/main/java/Migrator.java
@@ -1,6 +1,7 @@
 import Neo4j.Neo4JDataFormat;
 import Neo4j.Neo4JNode;
 import Neo4j.Neo4JRelation;
+import Neo4j.Neo4JWriter;
 import RelationalDB.EventsWorkbenchGetter;
 
 import java.sql.ResultSet;
@@ -10,14 +11,28 @@ import java.util.List;
 
 public class Migrator {
 
+    private static Neo4JWriter neow;
 
     public static void main(String[] args){
 
         try {
-            EventsWorkbenchGetter wb = new EventsWorkbenchGetter(args[0],args[1],args[2]);
-            Neo4JDataFormat.setInsertSize(10);
-            Neo4JDataFormat.setThreadCount(5);
-            Neo4JDataFormat.setConnection(args[3],args[4],args[5]);
+            if(args.length < 6 ){
+                System.out.println(" Insufficient number of arguments ");
+                System.out.println("arguments should be :");
+                System.out.println("<sqluser> <sqlpassword> <sqlip> <neo4juser> <neo4jpassword> <neo4jip>");
+                System.out.println("user sqluserpw localhost:3306 user neo4juserpw localhost:11001");
+                return;
+            }
+
+            String sqluser = args[0];
+            String sqlpassword = args[1];
+            String sqlip = args[2];
+            String neo4juser = args[3];
+            String neo4jpassword = args[4];
+            String neo4jip = args[5];
+
+            EventsWorkbenchGetter wb = new EventsWorkbenchGetter(sqluser,sqlpassword,sqlip);
+            neow = new Neo4JWriter(neo4juser,neo4jpassword,neo4jip,10);
 
             List<String> eventlist = new ArrayList<>();
             eventlist.add("id");eventlist.add("nome");//eventlist.add("preco");
@@ -35,6 +50,8 @@ public class Migrator {
             partlist.add("id");partlist.add("nome");partlist.add("email");partlist.add("telemovel");
             partlist.add("genero");partlist.add("nif");partlist.add("DataDeNascimento");
             transfer_table(partlist,wb.participante(),"Participante");
+
+            neow.halt();
 
             List<String> participalist = new ArrayList<>();
             participalist.add("preco");
@@ -55,7 +72,7 @@ public class Migrator {
                     "Participante");
 
             wb.termina();
-            Neo4JDataFormat.closeConnection();
+            neow.close();
 
 
         }catch(SQLException|InterruptedException a ){
@@ -70,14 +87,14 @@ public class Migrator {
                                               String relationship, String from ,
                                               String to) throws SQLException{
         while(table.next())
-            Neo4JDataFormat.queue(fillrelationship(atributes ,table ,relationship ,
+            neow.queue(fillrelationship(atributes ,table ,relationship ,
                     from ,to ));
     }
 
     private static void transfer_table(List<String> atributes, ResultSet table,
                                        String nodetype) throws SQLException{
         while(table.next())
-            Neo4JDataFormat.queue(fillnode(atributes, table, nodetype));
+            neow.queue(fillnode(atributes, table, nodetype));
     }
 
     private static Neo4JRelation fillrelationship(List<String> atributes,
--- Migration/src/main/java/Neo4j/Neo4JDataFormat.java
+++ Migration/src/main/java/Neo4j/Neo4JDataFormat.java
@@ -6,55 +6,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 
 public abstract class Neo4JDataFormat implements Runnable{
-    private static final Queue<Neo4JDataFormat> q = new LinkedList();
-    private static int n = 10;
-    private static int threadcount = 1;
-    private static List<Thread> th = new LinkedList<>();
-    private static Neo4JWriter w;
-    private static AtomicBoolean operating = new AtomicBoolean(true);
-
-    public static void setInsertSize(int n){
-        Neo4JDataFormat.n = n;
-    }
-
-    public static void setThreadCount(int n){
-        Neo4JDataFormat.threadcount = n;
-    }
-
-    public static void setConnection(String user, String password,
-                                     String ip) throws InterruptedException {
-        closeConnection();
-
-        operating.set(true);
-
-        w = new Neo4JWriter(user,password,ip);
-
-        for(int i=0; i< n; i++) {
-            Thread t = new Thread(new Neo4JNode("empty"));
-            th.add(t);
-            t.start();
-        }
-    }
-
-    public static void closeConnection() throws InterruptedException{
-
-        operating.set(false);
-
-        synchronized(q) {
-            q.notifyAll();
-        }
-
-        for(Thread t : th)
-            t.join();
-
-        q.clear();
-    }
-
-    public static void queue(Neo4JDataFormat a){
-        synchronized (q){
-            q.add(a);
-        }
-    }
 
     private final String name;
     private final Map<String,String> atributes = new HashMap();
@@ -77,32 +28,5 @@ public abstract class Neo4JDataFormat implements Runnable{
         atributes.put(type,attribute);
     }
 
-    public void run(){
 
-        try {
-            while(operating.get()){
-                List<Neo4JDataFormat> l = new ArrayList<>();
-                synchronized (q) {
-                    while (q.isEmpty() && operating.get())
-                        q.wait();
-
-
-                    Iterator<Neo4JDataFormat> it = q.iterator();
-                    for(int i = 0; i < threadcount; i++){
-                        if(it.hasNext()){
-                            l.add(it.next());
-                            it.remove();
-                        }else {
-                                break;
-                        }
-                    }
-                }
-                w.write(l);
-            }
-
-        }catch (InterruptedException e){
-            System.out.println(" worker terminated ");
-        }
-
-    }
 }
